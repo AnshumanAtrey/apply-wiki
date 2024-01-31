a667814a041a246a42bcb6b04b87c0a2c8f907e3
Anshuman Atrey <anshumanatrey@gmail.com>
2024-01-31 23:35:33 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+function App() {
+  return (
+    <>
+      <div className="w-20 bg-stone-950">
+        <p>21 Jan - 20 Jul</p>
+        <div>
+          <img src="" alt="" srcset="" />
+          <H1></H1>
+        </div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </>
+  );
+}
+
+export default App;
